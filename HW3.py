6a3637d0cf7c63b679d48dc191697064371c3fdc
Khapb <79282828+Khapb@users.noreply.github.com>
2021-02-28 12:04:46 +0200

    Update HW3.py
    Chenge HW3

--- HW3.py
+++ HW3.py
@@ -13,7 +13,7 @@ print(isinstance(int_a, str), isinstance(str_b, str), isinstance(set_c, list), i
 print("Anna has {1} apples and {0} peaches.". format(12, 7))
 print("Anna has {} apples and {} peaches.". format(12, 7))
 print("Anna has {apples} apples and {peaches} peaches.". format(apples=12, peaches=7))
-print("Anna has {0:5} apples and {1:3} peaches.". format(12, 7))
+print("Anna has {apples:.5} apples and {peaches:.3} peaches.". format(apples = "twelwe", peaches = "seven"))
 
 a = 5
 b = "apples"
